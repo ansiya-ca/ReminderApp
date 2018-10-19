eb2c3b5445fad2f60b4d2ea6c4b69fff6f39fab0
Muhammed Saifal <Muhammed.Saifal@dh.com>
2018-10-19 16:57:08 +0530

    first commit

---
+++ ReminderPro/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace ReminderPro
+{
+    partial class ReminderPlus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgReminders = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.dtpReminderDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.cmbTime = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReminders)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgReminders
+            // 
+            this.dgReminders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgReminders.Location = new System.Drawing.Point(12, 310);
+            this.dgReminders.Name = "dgReminders";
+            this.dgReminders.ReadOnly = true;
+            this.dgReminders.RowTemplate.Height = 24;
+            this.dgReminders.Size = new System.Drawing.Size(776, 260);
+            this.dgReminders.TabIndex = 0;
+            this.dgReminders.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgReminders_RowHeaderMouseClick);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(113, 21);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(274, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(125, 94);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(274, 49);
+            this.txtDesc.TabIndex = 2;
+            this.txtDesc.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dtpReminderDate
+            // 
+            this.dtpReminderDate.Location = new System.Drawing.Point(113, 135);
+            this.dtpReminderDate.Name = "dtpReminderDate";
+            this.dtpReminderDate.Size = new System.Drawing.Size(274, 22);
+            this.dtpReminderDate.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Date and Time";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.cmbTime);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtpReminderDate);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 247);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create/Update Reminder";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(261, 169);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(126, 34);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Create";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // cmbTime
+            // 
+            this.cmbTime.DisplayMember = "10 AM";
+            this.cmbTime.FormattingEnabled = true;
+            this.cmbTime.Items.AddRange(new object[] {
+            "12 AM",
+            "01 AM",
+            "02 AM",
+            "03 AM",
+            "04 AM",
+            "05 AM",
+            "06 AM",
+            "07 AM",
+            "08 AM",
+            "09 AM",
+            "10 AM",
+            "11 AM",
+            "12 PM",
+            "01 PM",
+            "02 PM",
+            "03 PM",
+            "04 PM",
+            "05 PM",
+            "06 PM",
+            "07 PM",
+            "08 PM",
+            "09 PM",
+            "10 PM",
+            "11 PM"});
+            this.cmbTime.Location = new System.Drawing.Point(113, 175);
+            this.cmbTime.Name = "cmbTime";
+            this.cmbTime.Size = new System.Drawing.Size(121, 24);
+            this.cmbTime.TabIndex = 6;
+            this.cmbTime.ValueMember = "10 AM";
+            this.cmbTime.SelectedIndexChanged += new System.EventHandler(this.cmbTime_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblDesc);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.lblTime);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.lblName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(446, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(297, 240);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Today\'s Event";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Reminder";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Red;
+            this.lblName.Location = new System.Drawing.Point(101, 47);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(14, 17);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "-";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.ForeColor = System.Drawing.Color.Red;
+            this.lblTime.Location = new System.Drawing.Point(101, 79);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(13, 17);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Time";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.BackColor = System.Drawing.Color.Red;
+            this.lblDesc.ForeColor = System.Drawing.Color.White;
+            this.lblDesc.Location = new System.Drawing.Point(101, 112);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(13, 17);
+            this.lblDesc.TabIndex = 5;
+            this.lblDesc.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Description";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(261, 209);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(126, 34);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(204, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "* select row to update reminder";
+            // 
+            // ReminderPlus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 582);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.dgReminders);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ReminderPlus";
+            this.Text = "ReminderPlus";
+            this.Load += new System.EventHandler(this.ReminderPlus_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgReminders)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgReminders;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.DateTimePicker dtpReminderDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ComboBox cmbTime;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ReminderPro/Form1.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReminderPro
+{
+    public partial class ReminderPlus : Form
+    {
+        private int reminderId;
+        private string updateId;
+        private string filepath = "../../db/reminder.json";
+        public ReminderPlus()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReminderPlus_Load(object sender, EventArgs e)
+        {
+            GetReminders();
+            cmbTime.SelectedIndex = 1;
+        }
+
+        private void GetReminders()
+        {
+            var jArray = GetData();
+            dgReminders.DataSource = jArray;
+            var lastItem = jArray.LastOrDefault();
+            if (lastItem != null)
+            {
+                reminderId = Convert.ToInt32(lastItem["Id"]);
+            }
+
+            reminderId = reminderId + 1;
+            lblName.Text = String.Empty;
+            lblTime.Text = String.Empty;
+            lblDesc.Text = String.Empty;
+            foreach (var remItem in jArray)
+            {
+                if (remItem["RemDate"].Value<string>() == DateTime.Today.ToShortDateString())
+                {
+                    lblName.Text = remItem["Name"].ToString();
+                    lblTime.Text = remItem["RemTime"].ToString();
+                    lblDesc.Text = remItem["Description"].ToString();
+                }
+            }
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            AddReminder();
+            ClearData();
+        }
+
+        private void ClearData()
+        {
+            txtName.Text = string.Empty;
+            txtDesc.Text = string.Empty;
+            dtpReminderDate.Value = DateTime.Today;
+            btnSubmit.Text = "Create";
+        }
+        private JArray GetData()
+        {
+            var json = File.ReadAllText(filepath);
+            var jArray = JArray.Parse(json);
+            return jArray;
+        }
+
+        private void AddReminder()
+        {
+            var newReminderData = "{ " + " 'Id': " + reminderId.ToString() + ", 'Name': '" + txtName.Text +
+                                  "', Description: '" + txtDesc.Text + "', RemDate: '" + dtpReminderDate.Value.ToShortDateString() + "', RemTime: '" + cmbTime.SelectedItem + "' } ";
+            var reminderArrary = GetData();
+            var newReminder = JObject.Parse(newReminderData);
+            foreach (var remItem in reminderArrary)
+            {
+                if (remItem["Id"].Value<string>() == updateId)
+                {
+                    int Id = remItem["Id"].Value<int>();
+                    newReminder["Id"] = Id;
+                    remItem["Name"] = newReminder["Name"];
+                    remItem["Description"] = newReminder["Description"];
+                    remItem["RemDate"] = newReminder["RemDate"];
+                    remItem["RemTime"] = newReminder["RemTime"];
+                }
+            }
+            if(btnSubmit.Text == "Create")
+            reminderArrary.Add(newReminder);
+
+            string newJsonResult =
+                Newtonsoft.Json.JsonConvert.SerializeObject(reminderArrary, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(filepath, newJsonResult);
+            GetReminders();
+        }
+
+        private void cmbTime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            dgReminders.ClearSelection();
+            ClearData();
+        }
+
+        private void dgReminders_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            updateId = dgReminders.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtName.Text = dgReminders.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtDesc.Text = dgReminders.Rows[e.RowIndex].Cells[2].Value.ToString();
+            dtpReminderDate.Value = Convert.ToDateTime(dgReminders.Rows[e.RowIndex].Cells[3].Value);
+            cmbTime.SelectedItem = dgReminders.Rows[e.RowIndex].Cells[4].Value.ToString();
+            btnSubmit.Text = "Update";
+
+        }
+    }
+    
+}
